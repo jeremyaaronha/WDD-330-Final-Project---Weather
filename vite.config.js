d06178bed76a15d81b65a1f291cc11c46b4e33f3
jeremyaaronha <jeremyaaronha@gmail.com>
2024-07-13 22:43:14 -0400

    fix path for viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'src', // Apuntar a la carpeta src
+  root: 'src',
   build: {
-    outDir: '../dist', // La carpeta de salida debe estar fuera de src
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: 'src/index.html',
+    },
   },
   server: {
     open: true
